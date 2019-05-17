32f18cfac69d0df621487eb15f7af430dcd6bb5d
FoxyStoat <carol.hodgson@hotmail.com>
2019-05-17 23:33:06 +0100

    feat: Add close button to close menu

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -1,6 +1,14 @@
 let mainNav = document.getElementById('js-menu');
 let navBarToggle = document.getElementById('js-navbar-toggle');
+var icon = document.getElementById('icon');
 
 navBarToggle.addEventListener('click', function () {
   mainNav.classList.toggle('active');
+  if (icon.classList.contains('fa-bars')) {
+		icon.classList.add('fa-times');
+		icon.classList.remove('fa-bars');
+	} else {
+			icon.classList.add('fa-bars');
+			icon.classList.remove('fa-times');
+		}
 });
